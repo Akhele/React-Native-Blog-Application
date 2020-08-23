7adee64971f0fb8144c2e685ad19b3ccc85be75e
Akhele <Moneimb8@gmail.com>
2020-08-23 18:48:09 +0100

    Adding State in Context

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, Button} from 'react-native';
+import BlogContext from '../context/BlogContext';
+import { FlatList } from 'react-native-gesture-handler';
 
 const indexScreen = () => {
+    const {data, addBlogPost} = useContext(BlogContext)
     return (
         <View>
             <Text>Index Screen</Text>
+
+            <FlatList
+                data={data}
+                keyExtractor={ (data) => data.title }
+                renderItem={
+                    ({item}) => {
+                    return <Text>{item.title}</Text>
+                    }
+                }
+            />
+            <Button title='Add Blog Post' onPress={addBlogPost} />
         </View>
     );
 };
