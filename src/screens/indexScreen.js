4b1a7cb2917e4b0bc489802258937658ad35b31d
Akhele <Moneimb8@gmail.com>
2020-08-27 12:52:06 +0100

    A bit of styling

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
 import {Context,Provider} from '../context/BlogContext';
 import { FlatList } from 'react-native-gesture-handler';
+import { Entypo } from '@expo/vector-icons'; 
+
 
 const indexScreen = () => {
     const {state, addBlogPost} = useContext(Context)
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title='Add Blog Post' onPress={addBlogPost} />
 
             <FlatList
@@ -16,7 +17,10 @@ const indexScreen = () => {
                 keyExtractor={ (data) => data.title }
                 renderItem={
                     ({item}) => {
-                    return <Text>{item.title}</Text>
+                    return <View style={styles.mainView}>
+                        <Text style={styles.title}>{item.title}</Text>
+                        <Entypo style={styles.icon} name="trash" size={24} color="black" />
+                        </View>
                     }
                 }
             />
@@ -25,6 +29,22 @@ const indexScreen = () => {
 };
 
 const styles = StyleSheet.create({
+    mainView : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1,
+        paddingHorizontal: 5
+         
+        
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+
+    }
 
 });
 
