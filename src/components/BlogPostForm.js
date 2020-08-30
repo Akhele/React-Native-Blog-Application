ba948808a52fa2daeb09223cd788e02f2793435f
Akhele <Moneimb8@gmail.com>
2020-08-30 20:37:02 +0100

    Extracting form logic

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,48 @@
+import React, {useContext, useState} from 'react';
+import { View, StyleSheet,Text,Button } from 'react-native';
+import {Context} from '../context/BlogContext'
+import { TextInput } from 'react-native-gesture-handler';
+
+const BlogPostForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+
+    return (
+    <View style={styles.mainView}>
+            <Text style={styles.label} >Title :</Text>
+            <TextInput value={title} onChangeText={(text) => {setTitle(text)}} style={styles.input} autoCorrect={false}/>
+            
+            <Text style={styles.label}>Content :</Text>
+            <TextInput value={content} onChangeText={(text) => {setContent(text)}} style={styles.input} autoCorrect={false} />
+
+            <Button title="Save" onPress={() => {
+                        // addBlogPost(title,content, () => {
+                        //     alert('Post Added Successfully')
+                        //     props.navigation.navigate('Index')
+                        // })
+                        
+                    }}/>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mainView : {
+        padding : 30
+    },
+    input: {
+        fontSize: 18,
+        borderColor: 'gray',
+        borderWidth: 1,
+        padding: 5,
+        marginBottom: 10
+    },
+    label: {
+        fontSize: 25,
+        marginBottom: 5
+    }
+});
+
+export default BlogPostForm;
--- src/screens/CreateBlog.js
+++ src/screens/CreateBlog.js
@@ -1,45 +1,18 @@
 import React, {useContext, useState} from 'react';
-import { View, StyleSheet,Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {Context} from '../context/BlogContext'
-import { TextInput } from 'react-native-gesture-handler';
+import BlogPostForm from '../components/BlogPostForm';
 
 const CreateBlog = (props) => {
+    
     const {addBlogPost} = useContext(Context);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    return (
-    <View style={styles.mainView}>
-            <Text style={styles.label} >Title :</Text>
-            <TextInput value={title} onChangeText={(text) => {setTitle(text)}} style={styles.input} autoCorrect={false}/>
-            
-            <Text style={styles.label}>Content :</Text>
-            <TextInput value={content} onChangeText={(text) => {setContent(text)}} style={styles.input} autoCorrect={false} />
 
-            <Button title="Add Blog Post" onPress={() => {
-                        addBlogPost(title,content, () => {
-                            alert('Post Added Successfully')
-                            props.navigation.navigate('Index')
-                        })
-                        
-                    }}/>
-    </View>
+    return (
+           <BlogPostForm/>
     );
 }
 const styles = StyleSheet.create({
-    mainView : {
-        padding : 30
-    },
-    input: {
-        fontSize: 18,
-        borderColor: 'gray',
-        borderWidth: 1,
-        padding: 5,
-        marginBottom: 10
-    },
-    label: {
-        fontSize: 25,
-        marginBottom: 5
-    }
+   
 });
 
 export default CreateBlog;
